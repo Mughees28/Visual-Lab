bca2b586f8c5b3fce55e6da371f4c03732fc7d65
Mughees28 <mugheesahmad68@gmail.com>
2024-06-03 02:26:43 +0500

    last commit

--- project/settings.py
+++ project/settings.py
@@ -99,8 +99,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'visualab_db',
-        'USER': 'root',
-        'PASSWORD': 'iamboss28',
+        'USER': 'root2',
+        'PASSWORD': 'jack123',
         'HOST': 'localhost',
         'PORT': '3306',
     }
