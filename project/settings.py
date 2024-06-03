aee280a432941ce70911536af19dc16d423cbfc8
Mughees28 <mugheesahmad68@gmail.com>
2024-06-03 13:24:56 +0500

    last commit

--- project/settings.py
+++ project/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         'NAME': 'visualab_db',
         'USER': 'root',
         'PASSWORD': 'iamboss28',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
