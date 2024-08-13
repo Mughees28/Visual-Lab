0b5e0bfc318bf3befb6e4e3d8470848702d7b477
Mughees28 <mugheesahmad68@gmail.com>
2024-08-13 15:07:49 +0500

    configured settings.py

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-hs-d0(e6#(a1n+%fkq=tih+dg8ucb)5n@c&tr-k8-hnogc%rm@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost',
     '127.0.0.1',
